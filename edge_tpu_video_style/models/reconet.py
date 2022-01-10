f63e75e5e54444cac52d1774070a849bc46e70a8
Sam Considine <sambconsidine@gmail.com>
2022-01-10 09:28:49 +0000

    Added TPU compatible instnorm

--- edge_tpu_video_style/models/reconet.py
+++ edge_tpu_video_style/models/reconet.py
@@ -11,31 +11,26 @@ from postprocessing.quantisation import quantise_model
 
 def resblock(x, kernelsize, filters):
     fx = layers.Conv2D(filters, kernelsize, activation='relu', padding='same')(x)
-    # fx = instance_normalisation(fx)
+    fx = instance_normalisation(fx)
     fx = layers.Conv2D(filters, kernelsize, padding='same')(fx)
+    x = instance_normalisation(x)
+    x = layers.ReLU()(x)
     out = layers.Add()([x,fx])
-    # out = instance_normalisation(out)
-    out = layers.ReLU()(out)
     return out
 
 
 def conv_instnorm_relu(x, filters, kernel_size, padding='same', strides=(1, 1)):
     x = layers.Conv2D(filters, kernel_size, strides=strides, padding=padding)(x)
-    # x = instance_normalisation(x)
-    # x = tf.keras.layers.BatchNormalization(axis=[1,2])(x) 
+    x = instance_normalisation(x)
     x = activations.relu(x)
     return x
     
 
 def instance_normalisation(x):
-    mean = tf.math.reduce_mean(x), #axis=(1, 2))
-    recip_stdev = tf.math.rsqrt(tf.math.reduce_sum(tf.math.squared_difference(x, mean)))#, axis=(1, 2)))
+    mean = tf.math.reduce_mean(x, axis=(1, 2))
+    recip_stdev = tf.math.rsqrt(tf.math.reduce_sum(tf.math.squared_difference(x, mean), axis=(1, 2)))
     normed = tf.multiply(tf.math.subtract(x, mean), recip_stdev)
-    return tf.math.subtract(x, mean)
-
-
-# def instance_normalisation(x):
-#     return tf.keras.layers.BatchNormalization(axis=[1,2])(x)
+    return normed
 
 
 def build_reconet() -> tf.keras.Model:
@@ -46,14 +41,16 @@ def build_reconet() -> tf.keras.Model:
     
     x = resblock(x, 3, 32)
     x = resblock(x, 3, 32)
-    # x = resblock(x, 3, 32)
-    # x = resblock(x, 3, 32)
-    # x = resblock(x, 3, 32)
+    x = resblock(x, 3, 32)
+    x = resblock(x, 3, 32)
+    x = resblock(x, 3, 32)
 
     features = x
 
     x = conv_instnorm_relu(x, 32, 3, padding='same')
-    # x = layers.Conv2D(64, 3, padding='same', activation='relu')(x)
+    x = tf.keras.layers.MaxPool2D()(x)
+    x = tf.keras.layers.UpSampling2D()(x)
+    x = layers.Conv2D(64, 3, padding='same', activation='relu')(x)
     x = layers.Conv2D(32, 9, padding='same', activation='tanh')(x)
     output = layers.Conv2D(3, 9, padding='same')(x)
 
